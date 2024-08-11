2a334d6faa3ca914fe3501deb00fa930eb9f80bc
Nahin Cevallos <njcevall@espol.edu.ec>
2024-08-10 23:24:11 -0500

    conexion de la base de datos con el Azure dentro del proyecto

--- AserrioSBD/src/main/java/ec/edu/espol/aserriosbd/DatabaseConnection.java
+++ AserrioSBD/src/main/java/ec/edu/espol/aserriosbd/DatabaseConnection.java
@@ -13,9 +13,9 @@ import java.sql.SQLException;
  * @author nicol
  */
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/AserrioDataPro";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://project-sbd.mysql.database.azure.com:3306/AserrioDataPro";
+    private static final String USER = "proyectoAserrio";
+    private static final String PASSWORD = "Basedatos1";
 
 
     public static Connection getConnection() {
