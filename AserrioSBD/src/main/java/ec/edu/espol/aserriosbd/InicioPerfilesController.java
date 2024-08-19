cb8895836649252f2a804cd3da5962cef3970f2a
Nahin Cevallos <njcevall@espol.edu.ec>
2024-08-19 04:47:46 -0500

    Actualizacion del diseño de interfaz

---
+++ AserrioSBD/src/main/java/ec/edu/espol/aserriosbd/InicioPerfilesController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.aserriosbd;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author nicol
+ */
+public class InicioPerfilesController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void mostrarDatos(MouseEvent event) {
+        try {
+            App.setRoot("opcionesSecretaria");
+        } catch (IOException ex) { 
+        }
+    }
+
+    
+    
+}
---
+++ AserrioSBD/src/main/java/ec/edu/espol/aserriosbd/OpcionesSecretariaController.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.aserriosbd;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author nicol
+ */
+public class OpcionesSecretariaController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void irMaquinaria(MouseEvent event) {
+        try {
+            App.setRoot("maquinaria");
+        } catch (IOException ex) { 
+        }
+    }
+
+    @FXML
+    private void irProducto(MouseEvent event) {
+        try {
+            App.setRoot("producto");
+        } catch (IOException ex) { 
+        }
+    }
+
+    @FXML
+    private void irCliente(MouseEvent event) {
+        try {
+            App.setRoot("cliente");
+        } catch (IOException ex) { 
+        }
+    }
+
+    @FXML
+    private void irProveedor(MouseEvent event) {
+        try {
+            App.setRoot("proveedor");
+        } catch (IOException ex) { 
+        }
+    }
+
+    @FXML
+    private void irLoteMadera(MouseEvent event) {
+        try {
+            App.setRoot("loteMadera");
+        } catch (IOException ex) { 
+        }
+    }
+    
+    @FXML
+    private void irEmpleado(MouseEvent event) {
+        try {
+            App.setRoot("empleado");
+        } catch (IOException ex) { 
+        }
+    }
+
+
+    @FXML
+    private void irTipoMadera(MouseEvent event) {
+        try {
+            App.setRoot("tipoMadera");
+        } catch (IOException ex) { 
+        }
+    }
+    
+}
